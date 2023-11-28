d9290cc2735f8af656ed590234f21a10fa2249c2
eralpavunduk <96149498+eralpavunduk@users.noreply.github.com>
2023-11-28 18:42:38 +0300

    arrangements made

--- Config/__init__.py
+++ Config/__init__.py
@@ -1,3 +1,3 @@
 data_set = 'ealaxi/paysim1'
-downloading_path = '/Users/eralp/OneDrive/Masaüstü/case2'
-reading_path = r"C:\Users\eralp\OneDrive\Masaüstü\case2\PS_20174392719_1491204439457_log.csv"
+downloading_path = 'where will your path downloading'
+reading_path = 'your path'
